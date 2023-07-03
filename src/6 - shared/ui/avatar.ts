1998649d82ea22c2c2820c34223c82faa88f0991
Vitaly Olhov <olivertears@gmail.com>
2023-07-03 21:31:36 +0200

    fix: avatar image path fix

--- src/6 - shared/ui/avatar.ts
+++ src/6 - shared/ui/avatar.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Avatar = styled.div`
-  background: url('../../../public/avatar.png') center;
+  background: url('avatar.png') center;
   background-size: 80%;
   width: 200px;
   height: 200px;
